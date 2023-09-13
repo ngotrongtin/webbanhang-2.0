09c41e6c9aa64c8aa8bc4d93c67f101143a7c9ae
ngotrongtin <tigopro.1703@gmail.com>
2023-09-13 10:46:56 +0700

    fix 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :carts, dependent: :destroy
   has_many :products, through: :carts
-  has_many :transactions, dependent: :destroy
+  has_many :transactions, dependent: :destroy, foreign_key: 'customer_id'
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 20, minimum: 1 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
